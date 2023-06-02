6b155d4f0503da4dc61a5dbd8349fc5a1541c6ef
James Gasek <jmg224@lehigh.edu>
2023-06-02 11:03:18 -0400

    Initial Commit

---
+++ GymPartner2/GymPartner2App.swift
@@ -0,0 +1,17 @@
+//
+//  GymPartner2App.swift
+//  GymPartner2
+//
+//  Created by James Gasek on 6/2/23.
+//
+
+import SwiftUI
+
+@main
+struct GymPartner2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
